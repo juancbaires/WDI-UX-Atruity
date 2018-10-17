1fde52fd7051521029d6dba4793fd5de6d540e1a
Felix Sawicki <fmsawicki@gmail.con>
2018-10-17 13:05:09 -0400

    css changes

--- strategic_planning/src/Sidebar/SideBar.js
+++ strategic_planning/src/Sidebar/SideBar.js
@@ -1,28 +1,36 @@
-import React, { Component } from 'react';
-import Bootstrap, { toggle } from "bootstrap-4-react"
-import "./SideBar.css"
+import React, { Component } from "react";
+import Bootstrap, { toggle } from "bootstrap-4-react";
+import "./SideBar.css";
 
 class SideBar extends Component {
-    render() {
-        return (
-            <div className="SideBar" >
-                <ul class="nav flex-column">
-                    <li class="nav-item">
-                        <a class="nav-link active" href="#">Active</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link disabled" href="#">Disabled</a>
-                    </li>
-                </ul>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="SideBar">
+        <ul class="nav flex-column">
+          <li class="nav-item">
+            <a class="nav-link active" href="#">
+              Active
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              Link
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              Link
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" href="#">
+              Disabled
+            </a>
+          </li>
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default SideBar;
