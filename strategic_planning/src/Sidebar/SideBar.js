c5596d51ed22112e5d18d1694f9d121e7b8e85d1
Juan Baires <juancbaires@gmail.com>
2018-10-17 13:04:18 -0400

    changes to app state and sidebar

--- strategic_planning/src/Sidebar/SideBar.js
+++ strategic_planning/src/Sidebar/SideBar.js
@@ -8,16 +8,25 @@ class SideBar extends Component {
             <div className="SideBar" >
                 <ul class="nav flex-column">
                     <li class="nav-item">
-                        <a class="nav-link active" href="#">Active</a>
+                        <a class="nav-link active" href="#">Step 1 Intro</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
+                        <a class="nav-link" href="#">Step 2 Mission/Vission</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
+                        <a class="nav-link" href="#">Step 3 SWOT</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link disabled" href="#">Disabled</a>
+                        <a class="nav-link" href="#">Step 4 Org Structure</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Step 5 Success Metrics</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Step 6 Goals</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Step 7 Executive Summary</a>
                     </li>
                 </ul>
             </div>
