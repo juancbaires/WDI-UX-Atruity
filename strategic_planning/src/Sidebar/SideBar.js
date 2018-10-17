f25ccf9827b270ef414ecf35c072ce59dc92fb57
Felix Sawicki <fmsawicki@gmail.con>
2018-10-17 13:54:54 -0400

    sidebar css

--- strategic_planning/src/Sidebar/SideBar.js
+++ strategic_planning/src/Sidebar/SideBar.js
@@ -3,49 +3,50 @@ import Bootstrap, { toggle } from "bootstrap-4-react";
 import "./SideBar.css";
 
 class SideBar extends Component {
-    render() {
-        return (
-            <div className="SideBar">
-                <ul class="nav flex-column">
-                    <li class="nav-item">
-                        <a class="nav-link active" href="#">
-                            Step 1 Intro
+  render() {
+    return (
+      <div className="SideBar">
+        <ul class="nav flex-column">
+          <li>Drop Down</li>
+          <li class="nav-item">
+            <a class="nav-link active" href="#">
+              Step 1 Intro
             </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">
-                            Step 2 Mission/Vission
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              Step 2 Mission/Vission
             </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">
-                            Step 3 SWOT
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              Step 3 SWOT
             </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">
-                            Step 4 Org Structure
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              Step 4 Org Structure
             </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">
-                            Step 5 Success Metrics
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              Step 5 Success Metrics
             </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">
-                            Step 6 Goals
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              Step 6 Goals
             </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">
-                            Step 7 Executive Summary
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              Step 7 Executive Summary
             </a>
-                    </li>
-                </ul>
-            </div>
-        );
-    }
+          </li>
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default SideBar;
