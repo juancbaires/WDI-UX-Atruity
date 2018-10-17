83cd77f4f8bd990e69fe3b3c78b739641ff7f422
Juan Baires <juancbaires@gmail.com>
2018-10-17 11:01:46 -0400

    created component folders

---
+++ strategic_planning/src/App/MainDisplay/MainDisplay.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class MainDisplay extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default MainDisplay;
