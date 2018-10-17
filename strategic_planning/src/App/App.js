73e0928dad3bf961cbeadfd674b3152c0f552939
Juan Baires <juancbaires@gmail.com>
2018-10-17 12:23:18 -0400

    css stuf

--- strategic_planning/src/App/App.js
+++ strategic_planning/src/App/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import "./App.css";
 import Header from "../Header/Header";
+import MainDisplay from "../MainDisplay/MainDisplay"
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
+        <MainDisplay />
       </div>
     );
   }
--- strategic_planning/src/Header/Header.js
+++ strategic_planning/src/Header/Header.js
@@ -1,37 +1,41 @@
 import React, { Component } from "react";
-import Bootstrap, { Button } from "bootstrap-4-react";
+import SideBar from "../Sidebar/SideBar"
 class Header extends Component {
   render() {
     return (
       <div>
-        <ul class="nav justify-content-center">
-          <li class="nav-item">
-            <a class="nav-link dashboard" href="#">
-              Dashboard
+        <header>
+          <ul class="nav justify-content-center">
+            <li class="nav-item">
+              <a class="nav-link dashboard" href="/">
+                Dashboard
             </a>
-          </li>
-          <li class="nav-item roadmap">
-            <a class="nav-link" href="#">
-              Roadmap
+            </li>
+            <li class="nav-item roadmap">
+              <a class="nav-link" href="/">
+                Roadmap
             </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link learning-hub" href="#">
-              Learning Hub
+            </li>
+            <li class="nav-item">
+              <a class="nav-link learning-hub" href="/">
+                Learning Hub
             </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link plan-builder" href="#">
-              Plan Builder
+            </li>
+            <li class="nav-item">
+              <a class="nav-link plan-builder" href="/">
+                Plan Builder
             </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link published-plans" href="#">
-              Published Plans
+            </li>
+            <li class="nav-item">
+              <a class="nav-link published-plans" href="/">
+                Published Plans
             </a>
-          </li>
-        </ul>
+            </li>
+          </ul>
+        </header>
+        <SideBar />
       </div>
+
     );
   }
 }
