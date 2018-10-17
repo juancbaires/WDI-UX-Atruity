96c37eda76dbcf66beb5cf542fc749c899ecaa09
Juan Baires <juancbaires@gmail.com>
2018-10-17 15:31:49 -0400

    added more comp

---
+++ strategic_planning/src/OrgStructure/OrgStructure.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class OrgStructure extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default OrgStructure;
---
+++ strategic_planning/src/SWOT/SWOT.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SWOT extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default SWOT;
