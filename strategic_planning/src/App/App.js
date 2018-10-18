4531b57f048bb9c232a45b197651fe09b83a0916
Felix Sawicki <fmsawicki@gmail.con>
2018-10-18 09:56:08 -0400

    css tweaks

--- strategic_planning/src/App/App.js
+++ strategic_planning/src/App/App.js
@@ -14,12 +14,12 @@ import OrgStructure from "../OrgStructure/OrgStructure";
 import SuccessMetrics from "../SuccessMetrics/SuccessMetric";
 import Goals from "../Goals/Goals";
 import ExecutiveSummary from "../ExecutiveSummary/ExecutiveSummary";
-import logo from "./logo.png"
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPen } from '@fortawesome/free-solid-svg-icons'
+import logo from "./logo.png";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPen } from "@fortawesome/free-solid-svg-icons";
 
-library.add(faPen)
+library.add(faPen);
 
 let planner = [
   {
@@ -45,56 +45,44 @@ class App extends Component {
     planner: planner
   };
   //add a goal to planner data
-  addplan = (plan) => {
-    this.state.planner[0].goals = plan
-  }
-  //edit mission prop 
-  addMission = (mission) => {
-    this.state.planner[0].visionStatement = mission
-  }
+  addplan = plan => {
+    this.state.planner[0].goals = plan;
+  };
+  //edit mission prop
+  addMission = mission => {
+    this.state.planner[0].visionStatement = mission;
+  };
   render() {
     return (
       <div className="App">
         <nav>
-          <img
-            class="nav justify-content-right"
-            src={logo}
-            alt="Atruity"
-          />
+          <img class="nav justify-content-right" src={logo} alt="Atruity" />
           <section className="centerLinks">
             <ul class="nav justify-content-center">
               <li class="nav-item">
                 <Link to="/dashboard">
                   {" "}
-                  <a class="nav-link dashboard" href="/">
-                    Dashboard
-                  </a>
+                  <p class="nav-link dashboard">Dashboard</p>
                 </Link>
               </li>
               <li class="nav-item roadmap">
                 <Link to="/roadmap">
-                  <a class="nav-link" href="/">
-                    Roadmap
-                  </a>
+                  <p class="nav-link">Roadmap</p>
                 </Link>
               </li>
               <li class="nav-item">
                 <Link to="/learninghub">
-                  <a class="nav-link learning-hub" href="/">
-                    Learning Hub
-                  </a>
+                  <p class="nav-link learning-hub">Learning Hub</p>
                 </Link>
               </li>
               <li class="nav-item">
                 <Link to="/planbuilder">
-                  <a class="nav-link plan-builder" href="/">
-                    Plan Builder
-                  </a>
+                  <p class="nav-link plan-builder">Plan Builder</p>
                 </Link>
               </li>
               <li class="nav-item">
                 <Link to="/publishedplans">
-                  <a class="nav-link published-plans">Published Plans</a>
+                  <p class="nav-link published-plans">Published Plans</p>
                 </Link>
               </li>
             </ul>
@@ -102,78 +90,101 @@ class App extends Component {
         </nav>
 
         <main>
-          <Switch>
-            <Route path="/executivesummary" render={props => (
-              <ExecutiveSummary
-                {...this.state}
-                {...this.props}
-                planner={this.state.planner}
+          <div>
+            <Switch>
+              <Route
+                path="/executivesummary"
+                render={props => (
+                  <ExecutiveSummary
+                    {...this.state}
+                    {...this.props}
+                    planner={this.state.planner}
+                  />
+                )}
+              />{" "}
               />
-            )}
-            /> />
-            <Route path="/goals" render={props => (
-              <Goals
-                {...this.state}
-                {...this.props}
-                planner={this.state.planner}
-                addplan={this.addplan}
+              <Route
+                path="/goals"
+                render={props => (
+                  <Goals
+                    {...this.state}
+                    {...this.props}
+                    planner={this.state.planner}
+                    addplan={this.addplan}
+                  />
+                )}
               />
-            )}
-            />/>
-            <Route path="/successmetrics" render={props => (
-              <SuccessMetrics
-                {...this.state}
-                {...this.props}
-                planner={this.state.planner}
               />
-            )}
-            /> />
-            <Route path="/orgstructure" render={props => (
-              <OrgStructure
-                {...this.state}
-                {...this.props}
-                planner={this.state.planner}
+              <Route
+                path="/successmetrics"
+                render={props => (
+                  <SuccessMetrics
+                    {...this.state}
+                    {...this.props}
+                    planner={this.state.planner}
+                  />
+                )}
+              />{" "}
               />
-            )}
-            />/>
-            <Route path="/swot" render={props => (
-              <SWOT
-                {...this.state}
-                {...this.props}
-                planner={this.state.planner}
+              <Route
+                path="/orgstructure"
+                render={props => (
+                  <OrgStructure
+                    {...this.state}
+                    {...this.props}
+                    planner={this.state.planner}
+                  />
+                )}
               />
-            )}
-            />/>
-            <Route path="/mission" render={props => (
-              <Mission
-                {...this.state}
-                {...this.props}
-                planner={this.state.planner}
-                addMission={this.addMission}
               />
-            )}
-            />/>
-            <Route
-              path="/planbuilder"
-              render={props => (
-                <PlanBuilder
-                  {...this.state}
-                  {...this.props}
-                  planner={this.state.planner}
-                />
-              )}
-            />
-            <Route path="/dashboard" component={Dashboard} />
-            <Route path="/learninghub" component={LearningHub} />
-            <Route path="/roadmap" component={RoadMap} />
-            <Route path="/publishedplans" render={props => (
-              <PublishedPlans
-                {...this.state}
-                {...this.props}
-                planner={this.state.planner}
+              <Route
+                path="/swot"
+                render={props => (
+                  <SWOT
+                    {...this.state}
+                    {...this.props}
+                    planner={this.state.planner}
+                  />
+                )}
               />
-            )} />
-          </Switch>
+              />
+              <Route
+                path="/mission"
+                render={props => (
+                  <Mission
+                    {...this.state}
+                    {...this.props}
+                    planner={this.state.planner}
+                    addMission={this.addMission}
+                  />
+                )}
+              />
+              />
+              <Route
+                path="/planbuilder"
+                render={props => (
+                  <PlanBuilder
+                    {...this.state}
+                    {...this.props}
+                    planner={this.state.planner}
+                  />
+                )}
+              />
+              <Route path="/dashboard" component={Dashboard} />
+              <Route path="/learninghub" component={LearningHub} />
+              <Route path="/roadmap" component={RoadMap} />
+              <Route
+                path="/publishedplans"
+                render={props => (
+                  <PublishedPlans
+                    {...this.state}
+                    {...this.props}
+                    planner={this.state.planner}
+                  />
+                )}
+              />
+            </Switch>
+          </div>
         </main>
       </div>
     );
--- strategic_planning/src/Sidebar/SideBar.js
+++ strategic_planning/src/Sidebar/SideBar.js
@@ -1,90 +1,108 @@
 import React, { Component } from "react";
 import Bootstrap, { toggle } from "bootstrap-4-react";
 import "./SideBar.css";
-import { Link, Route, Switch } from "react-router-dom"
-import Mission from "../Mission/Mission"
+import { Link, Route, Switch } from "react-router-dom";
+import Mission from "../Mission/Mission";
 
 class SideBar extends Component {
-    render() {
-        return (
-            <div className="SideBar">
-                <ul class="nav flex-column">
-                    <li class="nav-item dropdown">
-                        <a
-                            class="nav-link dropdown-toggle"
-                            href="#"
-                            id="navbarDropdown"
-                            role="button"
-                            data-toggle="dropdown"
-                            aria-haspopup="true"
-                            aria-expanded="false"
-                        >
-                            Plan Type
+  render() {
+    return (
+      <div className="SideBar">
+        <ul className="sideItem" class="nav flex-column">
+          <li class="nav-item dropdown">
+            <a
+              class="nav-link dropdown-toggle"
+              href="#"
+              id="navbarDropdown"
+              role="button"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >
+              Plan Type
             </a>
-                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="#">
-                                Strategic Plan
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <a class="dropdown-item" href="#">
+                Strategic Plan
               </a>
-                            <a class="dropdown-item" href="#">
-                                Startup Plan
+              <a class="dropdown-item" href="#">
+                Startup Plan
               </a>
-                            <a class="dropdown-item" href="#">
-                                Internal Business Plan
+              <a class="dropdown-item" href="#">
+                Internal Business Plan
               </a>
-                            <a class="dropdown-item" href="#">
-                                Feasibility Plan
+              <a class="dropdown-item" href="#">
+                Feasibility Plan
               </a>
-                            <a class="dropdown-item" href="#">
-                                Operations Plan
+              <a class="dropdown-item" href="#">
+                Operations Plan
               </a>
-                            <a class="dropdown-item" href="#">
-                                Growth Plan
+              <a class="dropdown-item" href="#">
+                Growth Plan
               </a>
-                        </div>
-                    </li>
-                    <li class="nav-item">
-                        <Link to="/planbuilder"><a class="nav-link active" href="#">
-                            Step 1- Intro
-            </a></Link>
-                    </li>
-                    <li class="nav-item">
-                        <Link to="/mission"><a class="nav-link" href="#">
-                            Step 2- Mission/Vission
-            </a></Link>
-                    </li>
-                    <li class="nav-item">
-                        <Link to="/swot"><a class="nav-link" href="#">
-                            Step 3- SWOT
-            </a></Link>
-                    </li>
-                    <li class="nav-item">
-                        <Link to="/orgstructure"><a class="nav-link" href="#">
-                            Step 4- Org Structure
-            </a></Link>
-                    </li>
-                    <li class="nav-item">
-                        <Link to="/successmetrics"><a class="nav-link" href="#">
-                            Step 5- Success Metrics
-            </a></Link>
-                    </li>
-                    <li class="nav-item">
-                        <Link to="/goals"><a class="nav-link" href="#">
-                            Step 6- Goals
-            </a></Link>
-                    </li>
-                    <li class="nav-item">
-                        <Link to="/executivesummary"><a class="nav-link" href="#">
-                            Step 7- Executive Summary
-            </a></Link>
-                    </li>
-                </ul>
-                <main>
-
-                </main>
-
             </div>
-        );
-    }
+          </li>
+
+          <li class="nav-item">
+            <Link to="/planbuilder">
+              <a class="nav-link active" href="#">
+                Step 1- Intro
+              </a>
+            </Link>
+          </li>
+
+          <li class="nav-item">
+            <Link to="/mission">
+              <a class="nav-link" href="#">
+                Step 2- Mission/Vission
+              </a>
+            </Link>
+          </li>
+
+          <li class="nav-item">
+            <Link to="/swot">
+              <a class="nav-link" href="#">
+                Step 3- SWOT
+              </a>
+            </Link>
+          </li>
+
+          <li class="nav-item">
+            <Link to="/orgstructure">
+              <a class="nav-link" href="#">
+                Step 4- Org Structure
+              </a>
+            </Link>
+          </li>
+
+          <li class="nav-item">
+            <Link to="/successmetrics">
+              <a class="nav-link" href="#">
+                Step 5- Success Metrics
+              </a>
+            </Link>
+          </li>
+
+          <li class="nav-item">
+            <Link to="/goals">
+              <a class="nav-link" href="#">
+                Step 6- Goals
+              </a>
+            </Link>
+          </li>
+
+          <li class="nav-item">
+            <Link to="/executivesummary">
+              <a class="nav-link" href="#">
+                Step 7- Executive Summary
+              </a>
+            </Link>
+          </li>
+        </ul>
+        <main />
+      </div>
+    );
+  }
 }
 
 export default SideBar;
